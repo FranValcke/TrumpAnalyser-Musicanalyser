9cd3c64c8ca5af2a6b846b21d11c2ba91c67ebba
FranValcke <fran.valcke@student.kdg.be>
2018-07-25 10:16:40 +0200

    Initial version

--- src/main/java/be/kdg/StartMain.java
+++ src/main/java/be/kdg/StartMain.java
@@ -6,7 +6,9 @@ import org.apache.spark.SparkContext;
 public class StartMain {
 
     public static void main(String[] args) {
+
         System.out.println("Test");
+        System.out.println("Test 2");
     }
 
 }
