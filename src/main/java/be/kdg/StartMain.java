64479bc829693d638dc9d2a3ee961357dba2fdf5
FranValcke <fran.valcke@student.kdg.be>
2018-07-31 11:41:43 +0200

    Initial version

--- src/main/java/be/kdg/StartMain.java
+++ src/main/java/be/kdg/StartMain.java
@@ -1,7 +1,5 @@
 package be.kdg;
 
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.function.FlatMapFunction;
 import org.apache.spark.api.java.function.Function;
@@ -10,12 +8,12 @@ import org.apache.spark.streaming.api.java.JavaDStream;
 import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 import org.apache.spark.streaming.twitter.TwitterUtils;
+import twitter4j.FilterQuery;
 import twitter4j.Status;
 import twitter4j.auth.Authorization;
 import twitter4j.auth.AuthorizationFactory;
 import twitter4j.conf.Configuration;
 import twitter4j.conf.ConfigurationContext;
-import org.apache.spark.streaming.twitter.*;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -25,57 +23,103 @@ import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
+
 public class StartMain {
 
     public static void main(String[] args) {
+        final String consumerKey = "KZSQvBjJpVBWvb97QHeL2pbYV";
+        final String consumerSecret = "6Auksnx9RNxON8XXldeiDnrTSgJOoGYjSW7R56sdyjRnJ8AotA";
+        final String accessToken = "948994158479409159-zgeiikgTqDbwbZ7s7iv5otmjeyZJ20F";
+        final String accessTokenSecret = "VluOhqkwvFUEpp7rwPehDV7IqWOdzETJ4wtxki7GZugqi";
 
-        System.out.println("Test");
+        SparkConf conf = new SparkConf().setMaster("local[2]").setAppName("MusicAnalyzer");
+        JavaStreamingContext jssc = new JavaStreamingContext(conf, new Duration(1000));
 
-        if (args.length < 4) {
-            System.err.println("Usage: JavaTwitterHashTagJoinSentiments <consumer key>" +
-                    " <consumer secret> <access token> <access token secret> [<filters>]");
-            System.exit(1);
-        }
-        /*//Zaken aanmaken via een twitterapp (Bron: youtube video https://www.youtube.com/watch?v=1GixYso8Az4&t=577s)
-        final String consumerKey = "";
-        final String consumerSecret = "";
-        final String accessToken = "";
-        final String accessTokenSecret = "";
-
-        String[] filters = {"#NowPlaying", "#nowplaying"};
-*/
-
-        String consumerKey = args[0];
-        String consumerSecret = args[1];
-        String accessToken = args[2];
-        String accessTokenSecret = args[3];
-        String[] filters = {"#NowPlaying"};
-
-        //Systeemproperties instellen zodat twitterstream gebruikt kan worden
         System.setProperty("twitter4j.oauth.consumerKey", consumerKey);
         System.setProperty("twitter4j.oauth.consumerSecret", consumerSecret);
         System.setProperty("twitter4j.oauth.accessToken", accessToken);
         System.setProperty("twitter4j.oauth.accessTokenSecret", accessTokenSecret);
 
-        //Sparkconfiguratie opstarten
-        //Local 2: we gaan local werken?
-        SparkConf conf = new SparkConf().setMaster("local[2]").setAppName("MusicAnalyser");
-        JavaStreamingContext jssc = new JavaStreamingContext(conf, new Duration(30000));
+        Configuration twitterConf = ConfigurationContext.getInstance();
+        Authorization twitterAuth = AuthorizationFactory.getInstance(twitterConf);
+
+        String[] filters = {"#NowPlaying", "#nowplaying"};
+
+        JavaReceiverInputDStream<Status> twitterStream = TwitterUtils.createStream(jssc, twitterAuth, filters);
+
+        JavaDStream<String> statusses = twitterStream.map(
+                new Function<Status, String>() {
+                    @Override
+                    public String call(Status status) {
+                        StringBuilder sb = new StringBuilder();
+
+                        String text = status.getText();
+                        String[] strArray = text.split(" ");
 
 
-        //Stream opstarten met de juiste filters
-        JavaReceiverInputDStream<Status> twitterStream = TwitterUtils.createStream(jssc, filters);
+                        //Patroon 1: #Nowplaying ... by ...
+                        if (strArray[0].startsWith("#")) {
+                            int teller = 0;
+                            int positie = 0;
+                            for (String s : strArray) {
+                                if (s.startsWith("by") || s.startsWith("-") || s.startsWith("door")) {
+                                    positie = teller;
+                                    break;
+                                }
+                                teller++;
+                            }
+                            // artist toevoegen
+                            sb.append(strArray[teller + 1]);
+                            sb.append("\n");
 
-        //Woorden er uit halen en deze in een array opslaan?
-        JavaDStream<String> words = twitterStream.flatMap(new FlatMapFunction<Status, String>() {
-            @Override
-            public Iterator<String> call(Status s) {
-                return Arrays.asList(s.getText().split(" ")).iterator();
-            }
-        });
+                            //String maken van de track
+                            StringBuilder title = new StringBuilder();
+                            for (int i = 1; i < positie; i++) {
+                                title = title.append(strArray[i]);
+                            }
+                            //track title toevoegen
+                            sb.append(title);
+                            sb.append("\n");
+                        }
 
-        words.dstream().saveAsTextFiles("file:///C:\\Users\\Fran\\Dropbox\\Toegepase informatica\\MD ETL BIGDATA NOSQL\\Examenproject\\testbestand", "txt\"");
+                        sb.append(status.getText());
 
+                        //artist
+                        // sb.append(strArray[artist]);
+                        sb.append("\n");
+                        //Tracktitle
+
+                        sb.append("\n");
+                        //Overigetekst
+
+                        //Taal
+                        sb.append(status.getLang());
+                        sb.append("\n");
+                        //user
+                        sb.append(status.getUser().getName());
+                        sb.append("\n");
+                        //timestamp
+                        sb.append(status.getCreatedAt());
+                        sb.append("\n");
+                        //TweetId
+                        sb.append(status.getId());
+                        sb.append("\n");
+                        //Hashtags
+                        String text1 = status.getText();
+                        String[] strArray1 = text1.split(" ");
+                        for (String s : strArray1) {
+                            if (s.startsWith("#")) {
+                                sb.append(s);
+                            }
+                        }
+
+
+                        return sb.toString();
+                    }
+                }
+        );
+
+        statusses.dstream().saveAsTextFiles("file:///C:/BigDataStreaming/MusicStream", "txt");
 
         jssc.start();
         try {
@@ -86,3 +130,5 @@ public class StartMain {
     }
 
 }
+
+
